55763575e804a8d20c9176966a6f789c7dc202af
Lorhan Sohaky <lorhansohaky@gmail.com>
2017-01-15 20:29:07 -0200

    Solução de um erro no calloc_string

--- src/dstring.c
+++ src/dstring.c
@@ -7,21 +7,22 @@
 #define DEFAULT_LENGTH 51// 50 for string + 1 for \0
 
 
-void calloc_string(String *string, unsigned capacity);
+String *calloc_string(String *string, unsigned capacity);
 
 String *string_new(){
     String *string=calloc(1,sizeof(String));
     if(string!=NULL){
-        calloc_string(string,DEFAULT_LENGTH);
+        string=calloc_string(string,DEFAULT_LENGTH);
     }
     return string;
 }
 
-void calloc_string(String *string, unsigned int capacity){
+String *calloc_string(String *string, unsigned int capacity){
     string->char_array=calloc(capacity,sizeof(char));
     if(string->char_array!=NULL){
         string->capacity=capacity;
     }else{
         string->capacity=0;
     }
+    return string;
 }
